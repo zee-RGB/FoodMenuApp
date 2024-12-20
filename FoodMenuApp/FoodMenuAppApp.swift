c0d91a42ae8ac3934a5fc5e0eb99ef9153d8b0fb
zee-RGB <zaahir85@gmail.com>
2024-12-21 07:26:53 +1000

    Initial Commit

---
+++ FoodMenuApp/FoodMenuAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FoodMenuAppApp.swift
+//  FoodMenuApp
+//
+//  Created by zee-RGB on 21/12/2024.
+//
+
+import SwiftUI
+
+@main
+struct FoodMenuAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
